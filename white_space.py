0dbaa70b7ea321858616780d08c4980930db83d8
rahulroxx <rahulyupz@gmail.com>
2016-05-14 23:14:27 +0530

    Minor refactoring

--- white_space.py
+++ white_space.py
@@ -21,24 +21,23 @@ def format_file(path):
     '''
     p = ""
     for dirpath, _, filenames in os.walk(path):
-        if (os.path.exists(os.path.join(dirpath, 'bin/activate'))):
+        if os.path.exists(os.path.join(dirpath, 'bin/activate')):
             p = dirpath
-        for f in filenames:
-            if f.endswith('.py'):
-                filename = os.path.abspath(os.path.join(dirpath, f))
-                if (not bool(p)):
-                    if os.access(filename, os.W_OK):
-                        try:
-                            file1 = open(filename, 'r')
-                            source_code = file1.read()
-                            file1.seek(0)
-                            file1 = open(filename, 'w')
-                            file1.truncate()
-                            file1.write(autopep8.fix_code(source_code))
-                            file1.close()
-                        except:
-                            print(filename)
-                            print(sys.exc_info()[0])
+        for file in filenames:
+            if file.endswith('.py'):
+                file = os.path.abspath(os.path.join(dirpath, file))
+                if (not bool(p)) and os.access(file, os.W_OK):
+                    try:
+                        temp_file = open(file, 'r')
+                        source_code = temp_file.read()
+                        temp_file.seek(0)
+                        temp_file = open(file, 'w')
+                        temp_file.truncate()
+                        temp_file.write(autopep8.fix_code(source_code))
+                        temp_file.close()
+                    except:
+                        print(file)
+                        print(sys.exc_info()[0])
 
 
 if __name__ == "__main__":
